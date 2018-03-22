3b6a1bf17eeb377bc2bef718247bc2e8ab4a5d87
GuttermanA <alexanderf.gutterman@gmail.com>
2018-03-22 14:45:25 -0400

    ensured that apiadapter could receive params

--- app/models/api_adapter.rb
+++ app/models/api_adapter.rb
@@ -1,6 +1,5 @@
 class ApiAdapter
   def self.search(search_term:)
-    byebug
     term = search_term.gsub(" ", "%").downcase
     url = "https://api.themoviedb.org/3/search/movie?query=#{term}&api_key=#{ENV['API_KEY']}"
     json = RestClient.get(url)
