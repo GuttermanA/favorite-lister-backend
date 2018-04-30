66be08ec4eaee3b919c6a5cc9a8d8b3c8a09763a
GuttermanA <alexanderf.gutterman@gmail.com>
2018-04-30 17:20:26 -0400

    removed auth controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-  # skip_before_action :authorized, only: [:create]
 
   def create
     @user = User.new(name: params[:name], password: params[:password])
