79f629e84622631866ecd8a3a11bfc9e0f70bbb8
GuttermanA <alexanderf.gutterman@gmail.com>
2018-04-30 16:54:37 -0400

    added figaro

--- Gemfile
+++ Gemfile
@@ -26,6 +26,7 @@ gem 'active_model_serializers'
 gem 'rack-cors'
 gem 'rest-client'
 gem 'jwt'
+gem 'figaro'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/models/api_adapter.rb
+++ app/models/api_adapter.rb
@@ -1,14 +1,16 @@
 class ApiAdapter
   def self.search(search_term:)
     term = search_term.gsub(" ", "%").downcase
-    url = "https://api.themoviedb.org/3/search/movie?query=#{term}&api_key=#{ENV['API_KEY']}"
+    url = "https://api.themoviedb.org/3/search/movie?query=#{term}&api_key=#{ENV['api_key']}"
     res = RestClient.get(url)
     JSON.parse(res)
   end
 
   def self.default
-    url = "https://api.themoviedb.org/3/discover/movie?api_key=#{ENV['API_KEY']}&include_adult=false&include_video=false&page=1&primary_release_year=#{Date.today.year}"
+    url = "https://api.themoviedb.org/3/discover/movie?api_key=#{ENV['api_key']}&include_adult=false&include_video=false&page=1&primary_release_year=#{Date.today.year}"
     res = RestClient.get(url)
     JSON.parse(res)
   end
+
+
 end
